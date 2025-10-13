d64de1a3e5bf4729f634ac99cb334634dd442497
viktor.volkov <volkov.vik1@gmail.com>
2025-10-13 12:50:03 +0300

    feat. Home work 2. Add ConcurrentDictionary with semaphore

--- Source/TspServer/TcpServer.cs
+++ Source/TspServer/TcpServer.cs
@@ -1,4 +1,5 @@
 ﻿using System.Buffers;
+using System.Collections.Concurrent;
 using System.Net;
 using System.Net.Sockets;
 
@@ -12,6 +13,8 @@ public class TcpServer : IDisposable
     private bool _isRunning;
     private bool _isDisposed;
     private Socket? _serverSocket;
+
+    private readonly ConcurrentDictionary<Socket, SemaphoreSlim> _semaphores = new();
 
     public async Task StartAsync(string ipAddress = "127.0.0.1", int port = 8080)
     {
@@ -35,8 +38,12 @@ public class TcpServer : IDisposable
         {
             try
             {
-                var clientSocket = await _serverSocket.AcceptAsync();
-                _ = Task.Run(async () => await ProcessClientAsync(clientSocket));
+                var clientSocket = await _serverSocket!.AcceptAsync();
+
+                var clientSemaphore = new SemaphoreSlim(1, 1);
+                _semaphores.TryAdd(clientSocket, clientSemaphore);
+
+                _ = Task.Run(async () => await ProcessClientAsync(clientSocket, clientSemaphore));
             }
             catch (SocketException ex)
             {
@@ -49,9 +56,9 @@ public class TcpServer : IDisposable
         }
     }
 
-    private static async Task ProcessClientAsync(Socket clientSocket)
+    private async Task ProcessClientAsync(Socket socket, SemaphoreSlim semaphore)
     {
-        Console.WriteLine($"TCP client {clientSocket.RemoteEndPoint} connected");
+        Console.WriteLine($"TCP client {socket.RemoteEndPoint} connected");
 
         var arrayPool = ArrayPool<byte>.Shared;
         var buffer = arrayPool.Rent(4096);
@@ -59,30 +66,80 @@ public class TcpServer : IDisposable
 
         try
         {
-            while (clientSocket.Connected)
+            while (socket.Connected)
             {
-                var bytesRead = await clientSocket.ReceiveAsync(memory, SocketFlags.None);
-                if (bytesRead < 1)
+                var result = await ProcessClientInternalAsync(socket, semaphore, memory);
+                if (!result)
                     break;
-
-                var readOnlySpan = memory[..bytesRead].Span;
-                var command = CommandParser<byte>.Parse(readOnlySpan, (byte)' ');
-                Console.WriteLine($"TCP client {clientSocket.RemoteEndPoint} send {command.ToString()}");
             }
         }
         catch (SocketException ex)
         {
-            Console.WriteLine($"TCP client socket error {clientSocket.RemoteEndPoint}: {ex.Message}");
+            Console.WriteLine($"TCP client socket error {socket.RemoteEndPoint}: {ex.Message}");
         }
         catch (Exception ex)
         {
-            Console.WriteLine($"TCP client error {clientSocket.RemoteEndPoint}: {ex.Message}");
+            Console.WriteLine($"TCP client error {socket.RemoteEndPoint}: {ex.Message}");
         }
         finally
         {
             arrayPool.Return(buffer);
 
-            CloseSocket(clientSocket);
+            CloseSemaphore(socket, semaphore);
+
+            CloseSocket(socket);
+        }
+    }
+
+    private static async Task<bool> ProcessClientInternalAsync(Socket socket, SemaphoreSlim semaphore, Memory<byte> memory)
+    {
+        var isAcquired = false;
+
+        try
+        {
+            isAcquired = await semaphore.WaitAsync(TimeSpan.FromMilliseconds(500));
+            if (!isAcquired)
+            {
+                Console.WriteLine($"TCP client {socket.RemoteEndPoint}. Semaphore capture timeout");
+                return true;
+            }
+
+            var bytesRead = await socket.ReceiveAsync(memory, SocketFlags.None);
+            if (bytesRead < 1)
+                return false;
+
+            var span = memory[..bytesRead].Span;
+            var command = CommandParser<byte>.Parse(span, (byte)' ');
+
+            Console.WriteLine($"TCP client {socket.RemoteEndPoint} send {command.ToString()}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"TCP client error {socket.RemoteEndPoint}: {ex.Message}");
+            return false;
+        }
+        finally
+        {
+            if (isAcquired)
+                semaphore.Release();
+        }
+        return true;
+    }
+
+
+    private void CloseSemaphore(Socket socket, SemaphoreSlim semaphore)
+    {
+        if (socket is null)
+            return;
+
+        try
+        {
+            _semaphores.TryRemove(socket, out _);
+            semaphore?.Dispose();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"TCP socket {socket.RemoteEndPoint}. Close semaphore error: {ex.Message}");
         }
     }
 
@@ -132,6 +189,16 @@ public class TcpServer : IDisposable
             _serverSocket?.Close();
             _serverSocket?.Dispose();
             _serverSocket = null;
+
+            foreach ((var socket, var semaphore) in _semaphores)
+            {
+                socket?.Shutdown(SocketShutdown.Both);
+                socket?.Close();
+                socket?.Dispose();
+
+                semaphore?.Dispose();
+            }
+            _semaphores.Clear();
 
             Console.WriteLine("TCP server stopped");
         }

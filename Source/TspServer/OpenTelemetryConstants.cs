9535e2bc73d08805e98730beb8889c5eafc2bf63
viktor.volkov <volkov.vik1@gmail.com>
2026-01-03 15:35:03 +0300

    feat. Home work 7. Add open telemetry

---
+++ Source/TspServer/OpenTelemetryConstants.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.Metrics;
+
+namespace TspServer;
+
+internal sealed class OpenTelemetryConstants
+{
+    // Имя сервиса
+    public const string ServiceName = "TCP server";
+    public const string ServiceVersion = "1.0.0";
+
+    // ActivitySource для трассировки
+    public static readonly ActivitySource MyActivitySource = new(ServiceName);
+
+    // Meter для метрик
+    public static readonly Meter MyMeter = new(ServiceName);
+}
